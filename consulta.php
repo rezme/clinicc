154bac4d2c1299bc513f852bea30a068d43570ca
luis <luis_rezme@hotmail.com>
2013-08-15 00:31:10 -0500

    consultabien

--- consulta.php
+++ consulta.php
@@ -1,26 +1,22 @@
 <?php 
 include "conexion.php";
 
-if(isset($_POST['id'])){
+if($_POST['id']!=null&&$_POST['nombre']!=null){
 	$id=$_POST['id'];
 	$nombre=$_POST['nombre'];
-	echo $nombre;
-	echo $id;
-	echo "lalala";
-	//$consulta=mysql_query("select * from respuesta where id_paciente='".$id."'");
-//	echo $consulta;
-//	if($id = mysql_fetch_array($consulta)){
-//		echo "<div class='alert alert-success'>";
-//		echo "existe su nombre es ".$id['nombre_paciente']." y su resultado es ".$id['respuesta'];
-//		echo "</div>";
-//	}else{
-//		echo "<div class='alert alert-danger'>";
-//		echo "noexiste";
-//		echo "</div>";
-//	}
-//}else{
-//	echo "<div class='alert alert-danger'>";
-//	echo "Parece que no enviaste los datos intentalo de nuevo";
-//	echo "</div>";
+	$consulta=mysql_query("select nombre_paciente,respuesta from repuesta where id_paciente='".$id."'");
+	if($id = mysql_fetch_array($consulta)){
+		echo "<div class='alert alert-success'>";
+		echo "Hola ".$id['nombre_paciente']." el resultado de tu estudio es: ".$id['respuesta'];
+		echo "</div>";
+	}else{
+		echo "<div class='alert alert-danger'>";
+		echo "El id o usuario que ingresaste no existen, por favor intenta de nuevo";
+		echo "</div>";
+	}
+}else{
+	echo "<div class='alert alert-danger'>";
+	echo "Upps! Parece que no enviaste los datos requeridos por favor intentalo de nuevo";
+	echo "</div>";
 }
 ?>
